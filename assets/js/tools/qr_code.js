d7d15bb9960dd097746de8edee579ee714891787
waibui <buivanyk4@gmail.com>
2024-10-24 23:23:56 +0700

    IO

--- assets/js/tools/qr_code.js
+++ assets/js/tools/qr_code.js
@@ -111,16 +111,10 @@ qrCodeFileInput.addEventListener('change', (event) => {
 
 dropZone.addEventListener('dragover', (event) => {
     event.preventDefault();
-    dropZone.classList.add('drag-over'); 
-});
-
-dropZone.addEventListener('dragleave', () => {
-    dropZone.classList.remove('drag-over'); 
 });
 
 dropZone.addEventListener('drop', (event) => {
     event.preventDefault(); 
-    dropZone.classList.remove('drag-over'); 
     const files = event.dataTransfer.files;
     if (files.length > 0) {
         readQrCode(files[0]);
